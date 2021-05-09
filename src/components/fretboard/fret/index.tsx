faad176d3b53d5662930e0a3ff88730c894679ea
Dave Nicholas <davenicholasuk@gmail.com>
2021-05-09 19:18:55 +0100

    some linting and add 9th fret

--- src/components/fretboard/fret/index.tsx
+++ src/components/fretboard/fret/index.tsx
@@ -10,21 +10,20 @@ interface Props {
     number: number;
 }
 
-const singleDotFrets = [3,5,7,15,17,19,21];
-const doubleDotFrets = [0,12];
+const singleDotFrets = [3, 5, 7, 9, 15, 17, 19, 21];
+const doubleDotFrets = [0, 12];
 
 export const Fret = ({ number }: Props) => {
-    
     const notes = [];
 
     const isSingleDot = singleDotFrets.includes(number);
     const isDoubleDot = doubleDotFrets.includes(number);
-    
-    const classes =  ClassNames({
-      "fret":true,
-      "fret--normal": !isSingleDot && !isDoubleDot,
-      "fret--single-dot": isSingleDot,
-      "fret--double-dot": isDoubleDot,
+
+    const classes = ClassNames({
+        fret: true,
+        'fret--normal': !isSingleDot && !isDoubleDot,
+        'fret--single-dot': isSingleDot,
+        'fret--double-dot': isDoubleDot,
     });
 
     for (let i = 0; i < NUMBER_OF_STRINGS; i++) {
